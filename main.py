cf1563ab266dcdac49f055796d9174a836337867
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-09-05 15:17:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-import asyncio
-from pytgcalls import idle
-from config import call_py, bot
+from pyrogram import Client
+from pytgcalls import PyTgCalls
 
+bot = Client("my_bot")  # Adjust with your bot name and configuration
+call_py = PyTgCalls(bot)  # Pass the pyrogram client to PyTgCalls
 
 async def main():
-    print("STARTING UBOT CLIENT")
-    await bot.start()
-    print("STARTING PYTGCALLS CLIENT")
-    await call_py.start()
-    print(
-        """
-    ------------------------
-   | MusicUserbot Actived! |
-    ------------------------
+    try:
+        print("STARTING UBOT CLIENT")
+        await bot.start()
+        print("STARTING PYTGCALLS CLIENT")
+        await call_py.start()
+        print(
+            """
+        ------------------------
+       | MusicUserbot Activated! |
+        ------------------------
 """
-    )
-    await idle()
-    print("STOPPING USERBOT")
-    await bot.stop()
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+        )
+        await idle()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        print("STOPPING USERBOT")
+        await bot.stop()
