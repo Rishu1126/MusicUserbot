5e369d58c71e58d816b51a1d8beb006e8a08e1d0
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-09-05 12:55:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ if os.path.exists(".env"):
 API_ID = int(os.getenv("API_ID", "24394477"))
 API_HASH = os.getenv("API_HASH", "b7bf7b4ddaf7c8f81c9e7575ad7a2ac1")
 SESSION = os.getenv("SESSION", "BQF0Ou0AapipsHULYsraEZnhODKmR7kSkZaKWuYXHaqSOTIawyd4kt6yB9OjdjKUqXlOMXXR9YW9v9B7gfve48lznfO9ZB1SaOUyPDVH1pyFTz7ezR1ZQbzPe6_CJ2pg7f8Fe1IvkmgLiJBNoA7lBJiQEJnR5EfwXZBD6KN4uqq20z3pwAlNLUyhuYFaRUjdmTO4pZR1wcKsKw1jUElvWH2SRPOel0QHfIEl31nX7SwaFdUyQkkxTS8AEoc1G0t1D9pHecj8nhMekMhcgqGAHDMUN3MqWH0xt2_s24AhqsSelsMDu6VsUGz3nBJx2tM5iZJGUEEdIJmzZIX2CYV3IjOSIbbTmgAAAAGWSmGJAA")
-HNDLR = os.getenv("HNDLR", "+", "!")
+HNDLR = os.getenv("HNDLR", "+")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", " 5516578116").split()))
 
 
