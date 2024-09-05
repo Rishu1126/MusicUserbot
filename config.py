6481fb4d1a2f151cde8cf97c2c15b52943a4725f
Rishu1126 <129749672+Rishu1126@users.noreply.github.com>
2024-09-05 14:59:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,5 +21,5 @@ contact_filter = filters.create(
     or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicUserbot"))
+bot = Client("Abhishek", API_ID, API_HASH, plugins=dict(root="MusicUserbot"))
 call_py = PyTgCalls(bot)
